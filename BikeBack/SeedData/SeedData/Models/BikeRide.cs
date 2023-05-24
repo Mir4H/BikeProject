95d997c1bce8fdeeb11978565a0e2af57baa9eeb
mir4H <mira.m.hamalainen@gmail.com>
2023-05-24 21:49:43 +0300

    added seeddata, models+packages

---
+++ BikeBack/SeedData/SeedData/Models/BikeRide.cs
@@ -0,0 +1,48 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeedData.Models
+{
+    public class BikeRide
+    {
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Name("Departure")]
+        public string DepartureTime { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        [Name("Return")]
+        public string ReturnTime { get; set; } = "";
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [Name("Departure station id")]
+        public int DepartureStationId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Departure station name")]
+        public string DepartureStationName { get; set; } = "";
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [Name("Return station id")]
+        public int ReturnStationId { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Return station name")]
+        public string ReturnStationName { get; set; } = "";
+
+        [Required]
+        [Range(10, float.MaxValue)]
+        [Name("Covered distance (m)")]
+        public float CoveredDistance { get; set; }
+
+        [Required]
+        [Range(10, float.MaxValue)]
+        [Name("Duration (sec.)")]
+        public float Duration { get; set; }
+    }
+}
---
+++ BikeBack/SeedData/SeedData/Models/BikeStation.cs
@@ -0,0 +1,69 @@
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace SeedData.Models
+{
+    public class BikeStation
+    {
+        [Range(1, int.MaxValue)]
+        [Name("FID")]
+        public int FID { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [Name("ID")]
+        public int StationID { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Nimi")]
+        public string FinnishName { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Namn")]
+        public string SwedishName { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Name")]
+        public string EnglishName { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Osoite")]
+        public string FinnishAddress { get; set; } = "";
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Name("Adress")]
+        public string SwedishAddress { get; set; } = "";
+
+        [DataType(DataType.Text)]
+        [Name("Kaupunki")]
+        public string FinnishCity { get; set; } = "";
+
+        [DataType(DataType.Text)]
+        [Name("Stad")]
+        public string SwedishCity { get; set; } = "";
+
+        [DataType(DataType.Text)]
+        [Name("Operaattor")]
+        public string Operator { get; set; } = "";
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        [Name("Kapasiteet")]
+        public int Capacity { get; set; }
+
+        [Required]
+        [Range(float.MinValue, float.MaxValue)]
+        [Name("x")]
+        public float LocationX { get; set; }
+
+        [Required]
+        [Range(float.MinValue, float.MaxValue)]
+        [Name("y")]
+        public float LocationY { get; set; }
+    }
+}
