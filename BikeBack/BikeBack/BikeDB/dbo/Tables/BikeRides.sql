8a1be84257316a6bd6ee3131f546cbb358c36a73
mir4H <mira.m.hamalainen@gmail.com>
2023-05-24 21:29:17 +0300

    created db tables

---
+++ BikeBack/BikeBack/BikeDB/dbo/Tables/BikeRides.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[BikeRides]
+(
+    [Id] INT NOT NULL IDENTITY(1,1) PRIMARY KEY, 
+    [DepartureTime] DATETIME NOT NULL, 
+    [ReturnTime] DATETIME NOT NULL, 
+    [DepartureStationId] INT NOT NULL, 
+    [DepartureStationName] NVARCHAR(50) NOT NULL,
+    [ReturnStationId] INT NOT NULL, 
+    [ReturnStationName] NVARCHAR(50) NOT NULL, 
+    [CoveredDistance] FLOAT NOT NULL CHECK (CoveredDistance>=10), 
+    [Duration] FLOAT NOT NULL CHECK (Duration>=10)
+)
---
+++ BikeBack/BikeBack/BikeDB/dbo/Tables/BikeStations.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[BikeStations]
+(
+    [StationID] INT NOT NULL PRIMARY KEY,  
+    [FinnishName] NVARCHAR(50) NOT NULL, 
+    [SwedishName] NVARCHAR(50) NOT NULL, 
+    [EnglishName] NVARCHAR(50) NOT NULL,
+    [FinnishAddress] NVARCHAR(50) NOT NULL, 
+    [SwedishAddress] NVARCHAR(50) NOT NULL, 
+    [FinnishCity] NVARCHAR(50) NULL, 
+    [SwedishCity] NVARCHAR(50) NULL, 
+    [Operator] NVARCHAR(50) NULL, 
+    [Capacity] INT NOT NULL, 
+    [LocationX] DECIMAL(15, 13) NOT NULL, 
+    [LocationY] DECIMAL(15, 13) NOT NULL
+)
